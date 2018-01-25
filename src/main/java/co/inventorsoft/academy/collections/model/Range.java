ee453ec4995117cbd8812a74ba553e4221cf3a84
Nina <napril96@gmail.com>
2018-01-25 13:09:12 +0200

    collections task

--- src/main/java/co/inventorsoft/academy/collections/model/Range.java
+++ src/main/java/co/inventorsoft/academy/collections/model/Range.java
@@ -1,33 +1,85 @@
 package co.inventorsoft.academy.collections.model;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
 
-public class Range<T> implements Set<T> {
+public class Range<T extends Comparable<T>> implements Set<T> {
+
+    private T start;
+    private T end;
+    private List<T> items;
+
+    public Range(T start, T end, Function<T, T> function) {
+        this.start = start;
+        this.end = end;
+        items = new ArrayList<>();
+        for (T i = start; i.compareTo(end) <= 0; i = function.apply(i)) {
+            items.add(i);
+        }
+    }
+
+    public static Range of(Integer start, Integer end) {
+        return new Range<>(start, end, new Function<Integer, Integer>() {
+            @Override
+            public Integer apply(Integer integer) {
+                return integer + 1;
+            }
+        });
+    }
+
+    public static Range of(Double start, Double end) {
+        return new Range<>(start, end, new Function<Double, Double>() {
+            @Override
+            public Double apply(Double num) {
+                return num + 0.1D;
+            }
+        });
+    }
+
+    public static Range of(Float start, Float end) {
+        return new Range<>(start, end, new Function<Float, Float>() {
+            @Override
+            public Float apply(Float num) {
+                return num + 0.1f;
+            }
+        });
+    }
+
+    public static Range of(Character start, Character end) {
+        return new Range<>(start, end, new Function<Character, Character>() {
+            @Override
+            public Character apply(Character character) {
+                return (char) (character + 1);
+            }
+        });
+    }
+
+    public static Range of(Character start, Character end, Function function) {
+        return new Range<>(start, end, function);
+    }
 
     public int size() {
-        return 0;
+        return items.size();
     }
 
     public boolean isEmpty() {
-        return false;
+        return items.size()==0;
     }
 
     public boolean contains(Object o) {
-        return false;
+        return items.contains(o);
     }
 
     public Iterator<T> iterator() {
-        return null;
+        return items.iterator();
     }
 
     public Object[] toArray() {
-        return new Object[0];
+        return items.toArray();
     }
 
     public <T1> T1[] toArray(T1[] a) {
-        return null;
+        return items.toArray(a);
     }
 
     public boolean add(T t) {
