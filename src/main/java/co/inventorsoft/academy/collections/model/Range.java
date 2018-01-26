fefb31a0b894e91d78af509ff6607488a76b42fb
spacex33 <3350serge@gmail.com>
2018-01-26 12:28:37 +0200

    range class implementation

--- src/main/java/co/inventorsoft/academy/collections/model/Range.java
+++ src/main/java/co/inventorsoft/academy/collections/model/Range.java
@@ -1,60 +1,46 @@
 package co.inventorsoft.academy.collections.model;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
 
-public class Range<T> implements Set<T> {
+public class Range<T extends Comparable> extends AbstractSet<T> implements Set<T> {
+
+    Set<T> set;
+
+    private Range(T first, T last, Function<T, T> function){
+        set = new TreeSet<T>();
+
+        if (first.compareTo(last) == 0){
+            return;
+        }
+
+        while (first.compareTo(last) <= 0){
+            set.add(first);
+            first = function.apply(first);
+        }
+    }
+
+    public static Range<Integer> of(Integer firstItem, Integer lastItem) {
+        return new Range<>(firstItem,lastItem, integer -> integer + 1);
+    }
+
+    public static Range<Float> of(Float firstItem, Float lastItem) {
+        return new Range<>(firstItem, lastItem, floatItem -> floatItem + 0.1f);
+    }
+
+    public static Range<Character> of(Character firstItem, Character lastItem, Function<Character, Character> function) {
+        return new Range<>(firstItem,lastItem,function);
+    }
 
     public int size() {
-        return 0;
+        return set.size();
     }
 
-    public boolean isEmpty() {
-        return false;
-    }
-
-    public boolean contains(Object o) {
-        return false;
+    public boolean add(T t){
+        return set.add(t);
     }
 
     public Iterator<T> iterator() {
-        return null;
-    }
-
-    public Object[] toArray() {
-        return new Object[0];
-    }
-
-    public <T1> T1[] toArray(T1[] a) {
-        return null;
-    }
-
-    public boolean add(T t) {
-        return false;
-    }
-
-    public boolean remove(Object o) {
-        return false;
-    }
-
-    public boolean containsAll(Collection<?> c) {
-        return false;
-    }
-
-    public boolean addAll(Collection<? extends T> c) {
-        return false;
-    }
-
-    public boolean retainAll(Collection<?> c) {
-        return false;
-    }
-
-    public boolean removeAll(Collection<?> c) {
-        return false;
-    }
-
-    public void clear() {
-
+        return set.iterator();
     }
 }
