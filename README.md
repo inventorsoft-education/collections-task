ce68d3c94103db638760e27335fb4f98dd15c731
Maksym Studenets <maksym.studenets@gmai.com>
2018-01-26 16:29:26 +0200

    Initial commit

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 https://docs.google.com/presentation/d/191qbQhSfUePOahkc7N0DkAQEJEQa9adX7WvuKAdtrlc/edit?usp=sharing
 
 # collections-task
-You need to design collection which represents a range. It needs to support all number based classes. It should be based on collection framework’s **Set** interface.  Float and double ranges should work with one digit prescission after comma. Custom types can be added via implementation of **Comparable**.
+You need to design collection which represents a range. It needs to support all number based classes.
+It should be based on collection framework’s **Set** interface.
+Float and double ranges should work with one digit prescission after comma.
+Custom types can be added via implementation of **Comparable**.
--- src/main/java/co/inventorsoft/academy/collections/model/Range.java
+++ src/main/java/co/inventorsoft/academy/collections/model/Range.java
@@ -1,60 +1,113 @@
 package co.inventorsoft.academy.collections.model;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.function.Function;
 
-public class Range<T> implements Set<T> {
+public class Range<T extends Comparable<T>> implements Set<T> {
+
+    private T from;
+    private T to;
+    private Function<T, T> increment;
+    private ArrayList<T> list = new ArrayList<>();
+
+    private Range(T start, T end, Function<T, T> increment) {
+        if (start.compareTo(end) <= 0) {
+            return;
+        }
+
+        if (end.compareTo(start) < 0) {
+            T temp = end;
+            end = start;
+            start = temp;
+        }
+
+        while (start.compareTo(end) < 0) {
+            list.add(start);
+            list.add(increment.apply(start));
+        }
+    }
+
+    public ArrayList<T> getList() {
+        return list;
+    }
 
     public int size() {
-        return 0;
+        return list.size();
     }
 
     public boolean isEmpty() {
-        return false;
+        return list.size() == 0;
     }
 
     public boolean contains(Object o) {
-        return false;
+        return list.contains(o);
     }
 
     public Iterator<T> iterator() {
-        return null;
+        return list.iterator();
     }
 
     public Object[] toArray() {
-        return new Object[0];
+        return list.toArray();
     }
 
     public <T1> T1[] toArray(T1[] a) {
-        return null;
+        return list.toArray(a);
     }
 
     public boolean add(T t) {
-        return false;
+        return list.contains(t) && list.add(t);
     }
 
     public boolean remove(Object o) {
-        return false;
+        return list.remove(o);
     }
 
     public boolean containsAll(Collection<?> c) {
-        return false;
+        return list.containsAll(c);
     }
 
+    @SuppressWarnings({"unchecked"})
     public boolean addAll(Collection<? extends T> c) {
         return false;
     }
 
     public boolean retainAll(Collection<?> c) {
-        return false;
+        return list.retainAll(c);
     }
 
     public boolean removeAll(Collection<?> c) {
-        return false;
+        return list.removeAll(c);
     }
 
     public void clear() {
+        list.clear();
+    }
 
+    public static <T extends Comparable<T>> Range<T> of(T start, T end, Function<T, T> increment) {
+        return new Range<>(start, end, increment);
+    }
+
+    public static Range<Short> of(Short from, Short to) {
+        return new Range<>(from, to, shortValue -> (short) (shortValue + 1));
+    }
+
+    public static Range<Integer> of(Integer from, Integer to) {
+        return new Range<>(from, to, intValue -> intValue + 1);
+    }
+
+    public static Range<Long> of(Long from, Long to) {
+        return new Range<>(from, to, longValue -> longValue + 1);
+    }
+
+    public static Range<Double> of(Double from, Double to) {
+        return new Range<>(from, to, doubleValue -> doubleValue + 1.0);
+    }
+
+    public static Range<Float> of (Float from, Float to) {
+        return new Range<>(from, to, floatValue -> floatValue + 1.0f);
     }
 }
