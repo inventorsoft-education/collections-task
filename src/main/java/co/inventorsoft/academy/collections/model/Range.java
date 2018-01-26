5b5c2f6290f841bf069a82654ae863f095ba0113
Nina <napril96@gmail.com>
2018-01-26 16:22:32 +0200

    another version of solution

--- src/main/java/co/inventorsoft/academy/collections/model/Range.java
+++ src/main/java/co/inventorsoft/academy/collections/model/Range.java
@@ -7,51 +7,28 @@ public class Range<T extends Comparable<T>> implements Set<T> {
 
     private T start;
     private T end;
-    private List<T> items;
+    private Function<T,T> function;
 
     public Range(T start, T end, Function<T, T> function) {
         this.start = start;
         this.end = end;
-        items = new ArrayList<>();
-        for (T i = start; i.compareTo(end) <= 0; i = function.apply(i)) {
-            items.add(i);
-        }
+        this.function = function;
     }
 
     public static Range of(Integer start, Integer end) {
-        return new Range<>(start, end, new Function<Integer, Integer>() {
-            @Override
-            public Integer apply(Integer integer) {
-                return integer + 1;
-            }
-        });
+        return new Range<>(start, end, integer -> integer + 1);
     }
 
     public static Range of(Double start, Double end) {
-        return new Range<>(start, end, new Function<Double, Double>() {
-            @Override
-            public Double apply(Double num) {
-                return num + 0.1D;
-            }
-        });
+        return new Range<>(start, end, aDouble -> aDouble + 0.1d);
     }
 
     public static Range of(Float start, Float end) {
-        return new Range<>(start, end, new Function<Float, Float>() {
-            @Override
-            public Float apply(Float num) {
-                return num + 0.1f;
-            }
-        });
+        return new Range<>(start, end, aFloat -> aFloat + 0.1f);
     }
 
     public static Range of(Character start, Character end) {
-        return new Range<>(start, end, new Function<Character, Character>() {
-            @Override
-            public Character apply(Character character) {
-                return (char) (character + 1);
-            }
-        });
+        return new Range<>(start, end, character -> (char) (character + 1));
     }
 
     public static Range of(Character start, Character end, Function function) {
@@ -59,39 +36,96 @@ public class Range<T extends Comparable<T>> implements Set<T> {
     }
 
     public int size() {
-        return items.size();
+        return toArray().length;
     }
 
     public boolean isEmpty() {
-        return items.size()==0;
+        return start.compareTo(end) == 0;
     }
 
     public boolean contains(Object o) {
-        return items.contains(o);
+        T t = (T) o;
+        return t.compareTo(start) > 0 && t.compareTo(end) < 0;
     }
 
     public Iterator<T> iterator() {
-        return items.iterator();
+        return new Iterator<T>() {
+            T cursor;
+            @Override
+            public boolean hasNext() {
+                T next;
+                if(cursor != null) {
+                     next = function.apply(cursor);
+                }else{
+                    next = start;
+                }
+                return next.compareTo(end) <= 0;
+            }
+
+            @Override
+            public T next() {
+                if(cursor == null){
+                    cursor = start;
+                    return cursor;
+                }
+                if(cursor.compareTo(end)>0){
+                    throw new NoSuchElementException();
+                }
+                cursor = function.apply(cursor);
+                return cursor;
+            }
+        };
     }
 
     public Object[] toArray() {
-        return items.toArray();
+        List<T> list = new ArrayList<>();
+        Iterator<T> iter = this.iterator();
+        while (iter.hasNext()){
+            list.add(iter.next());
+        }
+        return list.toArray();
     }
 
     public <T1> T1[] toArray(T1[] a) {
-        return items.toArray(a);
+        List<T> list = new ArrayList<>();
+        Iterator<T> iter = this.iterator();
+        while (iter.hasNext()){
+            list.add(iter.next());
+        }
+        return list.toArray(a);
     }
 
     public boolean add(T t) {
-        return false;
+        if(t.compareTo(start) > 0){
+            if(t.compareTo(end) < 0){
+                return false;
+            }
+            end = t;
+            return true;
+        }else {
+            start = t;
+            return true;
+        }
     }
 
     public boolean remove(Object o) {
-        return false;
+        T t = (T) o;
+        if(t.compareTo(start) > 0){
+            if(t.compareTo(end) < 0){
+                end = t;
+                return true;
+            }
+            return false;
+        }else {
+            return false;
+        }
     }
 
     public boolean containsAll(Collection<?> c) {
-        return false;
+         return c.stream()
+                 .map(this::contains)
+                 .filter(elem -> elem)
+                 .count() == c.size();
     }
 
     public boolean addAll(Collection<? extends T> c) {
@@ -107,6 +141,13 @@ public class Range<T extends Comparable<T>> implements Set<T> {
     }
 
     public void clear() {
+        end = start;
+    }
 
+    public static void main(String[] args) {
+        Range.of(1, 5).forEach(System.out::println);
+        Range.of('a', 'f').forEach(System.out::println);
+        Range<Integer> range = Range.of(1,  8);
+        System.out.println(range.containsAll(Arrays.asList(3,5,7,9)));
     }
 }
