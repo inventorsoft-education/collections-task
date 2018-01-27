d3f0c034b95d503da6082c422ce92ded7ab2fb15
spacex33 <3350serge@gmail.com>
2018-01-27 14:55:28 +0200

    add new solution

--- src/main/java/co/inventorsoft/academy/collections/model/Range.java
+++ src/main/java/co/inventorsoft/academy/collections/model/Range.java
@@ -5,19 +5,15 @@ import java.util.function.Function;
 
 public class Range<T extends Comparable> extends AbstractSet<T> implements Set<T> {
 
-    Set<T> set;
+    private T first;
+    private T last;
+    private Function<T,T> function;
 
     private Range(T first, T last, Function<T, T> function){
-        set = new TreeSet<T>();
 
-        if (first.compareTo(last) == 0){
-            return;
-        }
-
-        while (first.compareTo(last) <= 0){
-            set.add(first);
-            first = function.apply(first);
-        }
+        this.first = first;
+        this.last = last;
+        this.function = function;
     }
 
     public static Range<Integer> of(Integer firstItem, Integer lastItem) {
@@ -32,15 +28,72 @@ public class Range<T extends Comparable> extends AbstractSet<T> implements Set<T
         return new Range<>(firstItem,lastItem,function);
     }
 
-    public int size() {
-        return set.size();
-    }
-
-    public boolean add(T t){
-        return set.add(t);
+    public boolean contains(Object o) {
+        T t = (T) o;
+        if(t == null){
+            return false;
+        } else {
+            return t.compareTo(first) >= 0 && t.compareTo(last) <= 0;
+        }
     }
 
     public Iterator<T> iterator() {
-        return set.iterator();
+        return new Iterator<T>() {
+
+            T current;
+
+            @Override
+            public boolean hasNext() {
+                if(current == null) {
+                    return first.compareTo(last) <= 0;
+                } else {
+                    T next = function.apply(current);
+                    return next.compareTo(last) <= 0;
+                }
+            }
+
+            @Override
+            public T next() {
+                if(current == null){
+                    current = first;
+                    return current;
+                }
+                if(current.compareTo(last)>0){
+                    return null;
+                }
+                current = function.apply(current);
+                return current;
+            }
+        };
     }
+
+    public int size() {
+        return toArray().length;
+    }
+
+    public boolean isEmpty() {
+        return first.compareTo(last) == 0;
+    }
+
+    public boolean add(T t) {
+        if(t.compareTo(first) > 0){
+            if(t.compareTo(last) < 0){
+                return false;
+            }
+            last = t;
+            return true;
+        } else {
+            first = t;
+            return true;
+        }
+    }
+
+    public Object[] toArray() {
+        List<T> list = new ArrayList<>();
+        for (T t : this) {
+            list.add(t);
+        }
+        return list.toArray();
+    }
+
 }
