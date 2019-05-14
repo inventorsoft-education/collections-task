467e6631668f47ae42c6a1a390c3f45b338c1c56
stanislav <stasgumenyk@gmail.com>
2019-05-14 15:28:47 +0300

    implemented Range

--- src/main/java/co/inventorsoft/academy/collections/model/Range.java
+++ src/main/java/co/inventorsoft/academy/collections/model/Range.java
@@ -3,58 +3,99 @@ package co.inventorsoft.academy.collections.model;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.TreeSet;
+import java.util.function.Function;
 
 public class Range<T> implements Set<T> {
 
+    private TreeSet<T> data;
+
+    public Range() {
+        data = new TreeSet<>();
+    }
+
+    public static Range<Integer> of(int e1, int e2){
+        Range<Integer> range = new Range<>();
+        if (e1 < e2){
+            for (Integer i = e1; i <= e2; i++){
+                range.add(i);
+            }
+        }
+        return range;
+    }
+
+    public static Range<Float> of(float e1, float e2){
+        Range<Float> range = new Range<>();
+        if (e1 < e2){
+            for (Float i = e1; i <= e2; i = i + 0.1f){
+                range.add(i);
+            }
+        }
+        return range;
+    }
+
+    public static Range<Character> of(char e1, char e2, Function function){
+        Range<Character> range = new Range<>();
+        if (e1 < e2){
+            char i = e1;
+            while(i <= e2){
+                range.add(i);
+                i = (Character) function.apply(i);
+            }
+        }
+        return range;
+    }
+
+
     public int size() {
-        return 0;
+        return data.size();
     }
 
     public boolean isEmpty() {
-        return false;
+        return data.isEmpty();
     }
 
     public boolean contains(Object o) {
-        return false;
+        return data.contains(o);
     }
 
     public Iterator<T> iterator() {
-        return null;
+        return data.iterator();
     }
 
     public Object[] toArray() {
-        return new Object[0];
+        return data.toArray();
     }
 
     public <T1> T1[] toArray(T1[] a) {
-        return null;
+        return data.toArray(a);
     }
 
     public boolean add(T t) {
-        return false;
+        return data.add(t);
     }
 
     public boolean remove(Object o) {
-        return false;
+        return data.remove(o);
     }
 
     public boolean containsAll(Collection<?> c) {
-        return false;
+        return data.containsAll(c);
     }
 
     public boolean addAll(Collection<? extends T> c) {
-        return false;
+        return data.addAll(c);
     }
 
     public boolean retainAll(Collection<?> c) {
-        return false;
+        return data.retainAll(c);
     }
 
     public boolean removeAll(Collection<?> c) {
-        return false;
+        return data.remove(c);
     }
 
     public void clear() {
-
+        data.clear();
     }
 }
