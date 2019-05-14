d8a53fe725cb982bd46fbc0d6f63ac1d644d5fbe
ifochka9519 <shpulka9519@gmail.com>
2019-05-14 10:45:55 +0300

    task Plishkina fixed 2.0

--- src/main/java/co/inventorsoft/academy/collections/model/Range.java
+++ src/main/java/co/inventorsoft/academy/collections/model/Range.java
@@ -3,175 +3,119 @@ package co.inventorsoft.academy.collections.model;
 import java.util.*;
 import java.util.function.Function;
 
-public class Range<T> implements Set<T>{
+public class Range<T> implements Set<T> {
 
-    static Object[] elementData = new Object[10];
-    static int modCount = 0;
+    private ArrayList<T> elementData  = new ArrayList<>();
+    private T to;
+    private T from;
 
-    static Number left;
-    static Number right;
-    boolean isEmpty = true;
+    public Range(T from, T to, Function<T, T> function) {
+        this.from = from;
+        this.to = to;
 
-    public static Range<Integer> of(Integer a, Integer b) {
-        Range<Integer> range = new Range<>();
-        range.clear();
-        left = a;
-        right = b;
-        for(Integer j = left.intValue(); j<=right.intValue(); j++){
+        do{
+            this.elementData.add(from);
+            from = function.apply(from);
+        }
+        while (!(from.equals(to)));
+        this.elementData.add(to);
+       /* while (function.apply(from)) {
+            this.elementData.add(from);
+        }*/
+    }
+
+
+    public static Range<Integer> of(Integer from, Integer to) {
+        Range<Integer> range = new Range<>(from, to, intValue -> intValue + 1);
+        range.from = from;
+        range.to = to;
+        for(Integer j = range.from; j<=range.to; j++){
             range.add(j);
         }
         return range;
 
     }
 
-    public static Range<Float> of(Float a, Float b) {
-        Range<Float> range = new Range<>();
-        range.clear();
-        left = a;
-        right = b;
-        for(Float j = left.floatValue(); j<=right.floatValue(); j=j+0.1f){
+    public static Range<Float> of(Float from, Float to) {
+        Range<Float> range = new Range<>(from, to, intValue -> intValue + 1);
+        range.from = from;
+        range.to = to;
+        for(Float j = from; j<=to; j=j+0.1f){
             range.add(j);
         }
         return range;
 
     }
 
-    public static Range<Character> of(char a, char d, Function<Character, Character> characterCharacterFunction) {
-        Range<Character> range  = new Range<>();
-        range.clear();
-        for(Character j = a; j<=d; j++){
-            range.add(j);
-        }
-        return range;
+    public static Range<Character> of(char from, char to, Function<Character, Character> characterCharacterFunction) {
+        return new Range<>(from, to, characterCharacterFunction);
     }
 
     public int size() {
-        return modCount;
+        return this.elementData.size();
     }
 
-    public boolean isEmpty() {
-        return this.isEmpty;
-    }
+    public boolean isEmpty() { return this.elementData.isEmpty();}
 
     public boolean contains(Object o) {
-        return java.util.Arrays.asList(elementData).indexOf(o) >= 0;
+        return this.elementData.indexOf(o) >= 0;
     }
 
     public Iterator<T> iterator() {
-        Iterator iterator = new Iterator() {
-            int cursor = 0;
-            int lastRet = -1;
-            @Override
-            public boolean hasNext() {
-                return cursor != modCount;
-            }
-
-            @Override
-            public Object next() {
-                int i = cursor;
-                if (i >= modCount)
-                    throw new NoSuchElementException();
-                if (i >= elementData.length)
-                    throw new ConcurrentModificationException();
-                cursor = i + 1;
-                return (Object) elementData[lastRet = i];
-            }
-        };
+        Iterator iterator = this.elementData.iterator();
         return iterator;
     }
 
     public Object[] toArray() {
-        return elementData;
+        return this.elementData.toArray();
     }
 
     public <T1> T1[] toArray(T1[] a) {
-        T1[] array = (T1[]) new Object[a.length];
-        int i = 0;
-        for(T1 element: a){
-            array[i] = element;
-            i++;
-        }
-        return array;
+        return this.elementData.toArray(a);
     }
 
 
     public boolean add(T t) {
 
-        if(java.util.Arrays.asList(elementData).indexOf(t) >= 0){
+        if(this.elementData.indexOf(t) >= 0){
             return false;
         }
 
-        elementData[modCount] = t;
-        modCount++;
-        if(this.isEmpty){
-            this.isEmpty = false;
-        }
-        if(modCount == 1){
-            this.isEmpty = true;
-        }
+        this.elementData.add(t);
         return true;
     }
 
     public boolean remove(Object o) {
 
-        int index = java.util.Arrays.asList(elementData).indexOf(o);
-        for (int i = index; i<modCount; i++){
-            elementData[i] = elementData[i+1];
-        }
-        modCount--;
-        return true;
+        return  this.elementData.remove(o);
     }
 
     public boolean containsAll(Collection<?> c) {
-        boolean flag = true;
-        for (Object element: c) {
-            if(java.util.Arrays.asList(elementData).indexOf(element) < 0){
-                flag = false;
-            }
-        }
-        return flag;
+        return this.elementData.containsAll(c);
     }
 
     public boolean addAll(Collection<? extends T> c) {
-        for(Object element:c){
-            if(java.util.Arrays.asList(elementData).indexOf(element) < 0){
-                elementData[modCount] = element;
-                modCount++;
-            }
-
-        }
-        return true;
+        return this.elementData.addAll(c);
     }
 
     public boolean retainAll(Collection<?> c) {
-        for(Object element:elementData){
-            if(java.util.Arrays.asList(c).indexOf(element) < 0){
-                this.remove(element);
-            }
-        }
-        return true;
+
+        return this.elementData.retainAll(c);
     }
 
     public boolean removeAll(Collection<?> c) {
-        for(Object element:c){
-            if(java.util.Arrays.asList(elementData).indexOf(element) >= 0){
-                this.remove(element);
-            }
-        }
-        return true;
+
+        return this.elementData.removeAll(c);
     }
 
     public void clear() {
-        for(int i = 0; i < modCount; i++){
-            elementData[i] = null;
-        }
-        modCount = 0;
+        this.elementData.clear();
     }
 
-    @Override
+   /* @Override
     public int compareTo(Object o) {
         if(this == o)
             return 0;
         return 1;
-    }
+    }*/
 }
