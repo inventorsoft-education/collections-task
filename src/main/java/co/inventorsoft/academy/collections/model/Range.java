3785064f7fdf372391ce72e289961a1288d3906a
SteVi <trozzato@gmail.com>
2019-10-08 18:27:40 +0300

    Initial

--- src/main/java/co/inventorsoft/academy/collections/model/Range.java
+++ src/main/java/co/inventorsoft/academy/collections/model/Range.java
@@ -1,60 +1,159 @@
 package co.inventorsoft.academy.collections.model;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.function.Function;
 
 public class Range<T> implements Set<T> {
 
+    private HashSet<T> hashSet;
+
+    public Range() {
+        hashSet = new HashSet<>();
+    }
+
     public int size() {
-        return 0;
+        return hashSet.size();
     }
 
     public boolean isEmpty() {
-        return false;
+        return hashSet.isEmpty();
     }
 
     public boolean contains(Object o) {
-        return false;
+        return hashSet.contains(o);
     }
 
     public Iterator<T> iterator() {
-        return null;
+        return hashSet.iterator();
     }
 
     public Object[] toArray() {
-        return new Object[0];
+        return hashSet.toArray();
     }
 
     public <T1> T1[] toArray(T1[] a) {
-        return null;
+        return hashSet.toArray(a);
     }
 
     public boolean add(T t) {
-        return false;
+        return hashSet.add(t);
     }
 
     public boolean remove(Object o) {
-        return false;
+        return hashSet.remove(0);
     }
 
     public boolean containsAll(Collection<?> c) {
-        return false;
+        return hashSet.containsAll(c);
     }
 
     public boolean addAll(Collection<? extends T> c) {
-        return false;
+        return hashSet.addAll(c);
     }
 
     public boolean retainAll(Collection<?> c) {
-        return false;
+        return hashSet.retainAll(c);
     }
 
     public boolean removeAll(Collection<?> c) {
-        return false;
+        return hashSet.removeAll(c);
     }
 
     public void clear() {
-
+        hashSet.clear();
     }
+
+    public static Range<Integer> of(Integer begin, Integer end) {
+        Range<Integer> range = new Range<>();
+
+        if (begin.compareTo(end) == 0) {
+            return range;
+        }
+
+        while (begin.compareTo(end) <= 0) {
+            range.add(begin);
+            begin++;
+        }
+
+        return range;
+    }
+
+    public static Range<Byte> of(Byte begin, Byte end) {
+        Range<Byte> range = new Range<>();
+
+        if (begin.compareTo(end) == 0) {
+            return range;
+        }
+
+        while (begin.compareTo(end) <= 0) {
+            range.add(begin);
+            begin++;
+        }
+
+        return range;
+    }
+
+    public static Range<Short> of(Short begin, Short end) {
+        Range<Short> range = new Range<>();
+
+        if (begin.compareTo(end) == 0) {
+            return range;
+        }
+
+        while (begin.compareTo(end) <= 0) {
+            range.add(begin);
+            begin++;
+        }
+
+        return range;
+    }
+
+    public static Range<Float> of(Float begin, Float end) {
+        Range<Float> range = new Range<>();
+
+        if (begin.compareTo(end) == 0) {
+            return range;
+        }
+
+        while (begin.compareTo(end) <= 0) {
+            range.add(begin);
+            begin = begin + 0.1f;
+        }
+
+        return range;
+    }
+
+    public static Range<Double> of(Double begin, Double end) {
+        Range<Double> range = new Range<>();
+
+        if (begin.compareTo(end) == 0) {
+            return range;
+        }
+
+        while (begin.compareTo(end) <= 0) {
+            range.add(begin);
+            begin = begin + 0.1;
+        }
+
+        return range;
+    }
+
+    public static <T extends Comparable> Range<T> of(T begin, T end, Function<T, T> i) {
+        Range<T> range = new Range<>();
+
+        if (begin.compareTo(end) == 0) {
+            return range;
+        }
+
+        while (begin.compareTo(end) <= 0) {
+            range.add(begin);
+            begin = i.apply(begin);
+        }
+
+        return range;
+    }
+
 }
