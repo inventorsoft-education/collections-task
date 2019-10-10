bd011a71c950c59d2ec259885b65a4478e91a24b
Vasyl Lelek <itevas@outlook.com>
2019-10-10 14:01:49 +0300

    added general solution for Number

--- src/main/java/co/inventorsoft/academy/collections/model/Range.java
+++ src/main/java/co/inventorsoft/academy/collections/model/Range.java
@@ -4,8 +4,6 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.*;
 import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Range<T> implements Set<T> {
 
@@ -40,8 +38,11 @@ public class Range<T> implements Set<T> {
     }
 
     public boolean add(T t) {
-        return list.stream()
-                .anyMatch(element -> element.equals(t));
+        if (list.stream().anyMatch(element -> element.equals(t))) {
+            return false;
+        } else {
+            return list.add(t);
+        }
     }
 
     public boolean remove(Object o) {
@@ -78,121 +79,49 @@ public class Range<T> implements Set<T> {
         return range;
     }
 
+    private static <T extends Number> Range<T> of(T first, T last, Function<T, T> function) {
+        Range<T> range = new Range<>();
+        if (first == last) {
+            return range;
+        } else {
+            while (!first.equals(last)) {
+                range.add(first);
+                first = function.apply(first);
+            }
+            range.add(first);
+            return range;
+        }
+    }
+
     static Range<Integer> of(int firstElement, int lastElement) {
-        Range<Integer> range = new Range<>();
-        if (firstElement == lastElement) {
-            return range;
-        } else {
-            range.addAll(Stream.iterate(firstElement, i -> i + 1)
-                    .limit(lastElement)
-                    .collect(Collectors.toList()));
-            return range;
-        }
+        return of(firstElement, lastElement, element -> element + firstElement);
     }
 
-    static Range<Float> of(float firstElement, float lastElement){
-        Range<Float> range = new Range<>();
-        float step = firstElement;
-        if(firstElement == lastElement){
-            return range;
-        } else {
-            while (firstElement != lastElement){
-                range.add(firstElement);
-                firstElement = firstElement + step;
-            }
-            range.add(firstElement);
-            return range;
-        }
+    static Range<Float> of(float firstElement, float lastElement) {
+        return of(firstElement, lastElement, element -> (element + firstElement));
     }
 
-    static Range<Short> of(short firstElement, short lastElement){
-        Range<Short> range = new Range<>();
-        short step = firstElement;
-        if(firstElement == lastElement){
-            return range;
-        } else {
-            while (firstElement != lastElement){
-                range.add(firstElement);
-                firstElement = (short)(firstElement + step);
-            }
-            range.add(firstElement);
-            return range;
-        }
+    static Range<Short> of(short firstElement, short lastElement) {
+        return of(firstElement, lastElement, element -> (short) (element + firstElement));
     }
 
-    static Range<Long> of(long firstElement, long lastElement){
-        Range<Long> range = new Range<>();
-        long step = firstElement;
-        if(firstElement == lastElement){
-            return range;
-        } else {
-            while (firstElement != lastElement){
-                range.add(firstElement);
-                firstElement = firstElement + step;
-            }
-            range.add(firstElement);
-            return range;
-        }
+    static Range<Long> of(long firstElement, long lastElement) {
+        return of(firstElement, lastElement, element -> element + firstElement);
     }
 
-    static Range<Double> of(double firstElement, double lastElement){
-        Range<Double> range = new Range<>();
-        double step = firstElement;
-        if(firstElement == lastElement){
-            return range;
-        } else {
-            while (firstElement != lastElement){
-                range.add(firstElement);
-                firstElement = firstElement + step;
-            }
-            range.add(firstElement);
-            return range;
-        }
+    static Range<Double> of(double firstElement, double lastElement) {
+        return of(firstElement, lastElement, element -> element + firstElement);
     }
 
-    static Range<Byte> of(byte firstElement, byte lastElement){
-        Range<Byte> range = new Range<>();
-        byte step = firstElement;
-        if(firstElement == lastElement){
-            return range;
-        } else {
-            while (firstElement != lastElement){
-                range.add(firstElement);
-                firstElement = (byte)(firstElement + step);
-            }
-            range.add(firstElement);
-            return range;
-        }
+    static Range<Byte> of(byte firstElement, byte lastElement) {
+        return of(firstElement, lastElement, element -> (byte) (element + firstElement));
     }
 
-    static Range<BigInteger> of(BigInteger firstElement, BigInteger lastElement){
-        Range<BigInteger> range = new Range<>();
-        BigInteger step = firstElement;
-        if(firstElement == lastElement){
-            return range;
-        } else {
-            while (firstElement != lastElement){
-                range.add(firstElement);
-                firstElement = firstElement.add(step);
-            }
-            range.add(firstElement);
-            return range;
-        }
+    static Range<BigInteger> of(BigInteger firstElement, BigInteger lastElement) {
+        return of(firstElement, lastElement, element -> element.add(firstElement));
     }
 
-    static Range<BigDecimal> of(BigDecimal firstElement, BigDecimal lastElement){
-        Range<BigDecimal> range = new Range<>();
-        BigDecimal step = firstElement;
-        if(firstElement == lastElement){
-            return range;
-        } else {
-            while (firstElement != lastElement){
-                range.add(firstElement);
-                firstElement = firstElement.add(step);
-            }
-            range.add(firstElement);
-            return range;
-        }
+    static Range<BigDecimal> of(BigDecimal firstElement, BigDecimal lastElement) {
+        return of(firstElement, lastElement, element -> element.add(firstElement));
     }
-
 }
