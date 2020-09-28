581f9d5cf5ada040568c55fb9482211bcd342091
Oleh Kilaru <kilaruoleh@gmail.com>
2020-09-28 13:39:26 +0300

    Range Collection was made

--- src/main/java/co/inventorsoft/academy/collections/model/Range.java
+++ src/main/java/co/inventorsoft/academy/collections/model/Range.java
@@ -1,60 +1,92 @@
 package co.inventorsoft.academy.collections.model;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
 
 public class Range<T> implements Set<T> {
 
+    private HashSet<T> mySet;
+
+    public Range() {
+        mySet = new HashSet<>();
+    }
+
     public int size() {
-        return 0;
+        return mySet.size();
     }
 
     public boolean isEmpty() {
-        return false;
+        return mySet.isEmpty();
     }
 
     public boolean contains(Object o) {
-        return false;
+        return mySet.contains(o);
     }
 
     public Iterator<T> iterator() {
-        return null;
+        return mySet.iterator();
     }
 
     public Object[] toArray() {
-        return new Object[0];
+        return mySet.toArray();
     }
 
     public <T1> T1[] toArray(T1[] a) {
-        return null;
+        return mySet.toArray(a);
     }
 
     public boolean add(T t) {
-        return false;
+        return mySet.add(t);
     }
 
     public boolean remove(Object o) {
-        return false;
+        return mySet.remove(o);
     }
 
     public boolean containsAll(Collection<?> c) {
-        return false;
+        return mySet.containsAll(c);
     }
 
     public boolean addAll(Collection<? extends T> c) {
-        return false;
+        return mySet.addAll(c);
     }
 
     public boolean retainAll(Collection<?> c) {
-        return false;
+        return mySet.retainAll(c);
     }
 
     public boolean removeAll(Collection<?> c) {
-        return false;
+        return mySet.removeAll(c);
     }
 
     public void clear() {
+        mySet.clear();
+    }
 
+    public static Range<Byte> of(byte first, byte second) {
+        return of(first, second, element -> (byte) (element + 1));
+    }
+
+    public static Range<Integer> of(int first, int second) {
+        return of(first, second, element -> (element + 1));
+    }
+
+    public static Range<Float> of(float first, float second) {
+        return of(first, second, element -> element + 0.1f);
+    }
+
+    public static Range<Long> of(long first, long second) {
+        return of(first, second, element -> element + 1);
+    }
+
+    public static <T extends Comparable> Range<T> of(T first, T second, Function<T, T> function) {
+        Range<T> range = new Range<>();
+        if (first != second) {
+            while (first.compareTo(second) <= 0) {
+                range.add(first);
+                first = function.apply(first);
+            }
+        }
+        return range;
     }
 }
