34d6b303c272f5d08bb74282f628dfa9cc4332e4
Oleh Khalus <oleh0704@gmail.com>
2020-09-30 21:20:10 +0300

    Update Range collection

--- src/main/java/co/inventorsoft/academy/collections/model/Range.java
+++ src/main/java/co/inventorsoft/academy/collections/model/Range.java
@@ -1,20 +1,11 @@
 package co.inventorsoft.academy.collections.model;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
 
 public class Range<T> implements Set<T> {
 
     private HashSet<T> range;
-    private static final Range<Byte> rByte = new Range();
-    private static final Range<Short> rShort = new Range();
-    private static final Range<Integer> rInt = new Range();
-    private static final Range<Long> rLong = new Range();
-    private static final Range<Double> rDouble = new Range();
-    private static final Range<Float> rFloat = new Range();
-//    private static final Range<Character> rChar = new Range();
 
     public Range(){
         range = new HashSet<T>();
@@ -76,6 +67,7 @@ public class Range<T> implements Set<T> {
     }
 
     public static Range<Byte> of( byte element1, byte element2){
+        Range<Byte> rByte = new Range<>();
         for(byte i=element1;i<=element2;i++){
             rByte.add(i);
         }
@@ -83,35 +75,50 @@ public class Range<T> implements Set<T> {
     }
 
     public static Range<Short> of( short element1, short element2){
+        Range <Short> rShort = new Range<Short>();
         for(short i=element1;i<=element2;i++){
             rShort.add(i);
         }
         return rShort;
     }
     public static Range<Integer> of( int element1, int element2){
+        Range<Integer> rInt = new Range<>();
         for(int i=element1;i<=element2;i++){
             rInt.add(i);
         }
         return rInt;
     }
     public static Range<Long> of( long element1, long element2){
+        Range<Long> rLong = new Range<>();
         for(long i=element1;i<=element2;i++){
             rLong.add(i);
         }
         return rLong;
     }
     public static Range<Double> of( double element1, double element2){
+        Range<Double> rDouble = new Range<>();
         for(double i=element1;i<=element2;i=i+0.1){
             rDouble.add(i);
         }
         return rDouble;
     }
     public static Range<Float> of( float element1, float element2){
+        Range<Float> rFloat = new Range<>();
         for(float i=element1;i<=element2;i=i+0.1f){
             rFloat.add(i);
         }
         return rFloat;
     }
 
-
+    static <T extends Comparable> Range<T> of(T start, T end, Function<T, T> function) {
+        Range<T> rT = new Range<>();
+        if (start.equals(end)) {
+            return rT;
+        }
+        while (start.compareTo(end)<=0) {
+            rT.add(start);
+            start = function.apply(start);
+        }
+        return rT;
+    }
 }
