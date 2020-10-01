efdf90fdaba5bb05ef807ec6022ab5d462c549b6
Oleh Khalus <oleh0704@gmail.com>
2020-10-01 21:56:30 +0300

    Remove wildcard`s import Delete all basic Range<> methods update Range<Integer> and Range<Float> methods

--- src/main/java/co/inventorsoft/academy/collections/model/Range.java
+++ src/main/java/co/inventorsoft/academy/collections/model/Range.java
@@ -1,6 +1,9 @@
 package co.inventorsoft.academy.collections.model;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.function.Function;
 
 public class Range<T> implements Set<T> {
@@ -16,9 +19,6 @@ public class Range<T> implements Set<T> {
     }
 
     public boolean isEmpty() {
-        if(size()==1){
-            return true;
-        }
         return range.isEmpty();
     }
 
@@ -66,44 +66,23 @@ public class Range<T> implements Set<T> {
         range.clear();
     }
 
-    public static Range<Byte> of( byte element1, byte element2){
-        Range<Byte> rByte = new Range<>();
-        for(byte i=element1;i<=element2;i++){
-            rByte.add(i);
-        }
-        return rByte;
-    }
 
-    public static Range<Short> of( short element1, short element2){
-        Range <Short> rShort = new Range<Short>();
-        for(short i=element1;i<=element2;i++){
-            rShort.add(i);
-        }
-        return rShort;
-    }
     public static Range<Integer> of( int element1, int element2){
         Range<Integer> rInt = new Range<>();
+        if(element1 == element2){
+            return rInt;
+        }
         for(int i=element1;i<=element2;i++){
             rInt.add(i);
         }
         return rInt;
     }
-    public static Range<Long> of( long element1, long element2){
-        Range<Long> rLong = new Range<>();
-        for(long i=element1;i<=element2;i++){
-            rLong.add(i);
-        }
-        return rLong;
-    }
-    public static Range<Double> of( double element1, double element2){
-        Range<Double> rDouble = new Range<>();
-        for(double i=element1;i<=element2;i=i+0.1){
-            rDouble.add(i);
-        }
-        return rDouble;
-    }
+
     public static Range<Float> of( float element1, float element2){
         Range<Float> rFloat = new Range<>();
+        if(element1 == element2){
+            return rFloat;
+        }
         for(float i=element1;i<=element2;i=i+0.1f){
             rFloat.add(i);
         }
